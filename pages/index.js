f66cf74b0e75b2ac17f54ad9711fa9650168dcd7
luis <lr7drum@gmail.com>
2022-06-23 19:42:04 -0300

    yasmim

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function home (){
-    return <div> <h1>Yasmmim meu amor</h1></div>
+    return <div> <h1>Yasmmim meu amor eu te amo</h1>
+    <h1>você é tudo pra mim tudo o que eu mais quero</h1></div>
     
 }
 export default home
